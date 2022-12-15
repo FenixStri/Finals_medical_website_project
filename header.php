b4822021a88dbc155698c8440335e25c49845eec
Nrupen07 <111888339+Nrupen07@users.noreply.github.com>
2022-12-15 09:17:38 -0600

    Update header.php

--- header.php
+++ header.php
@@ -8,37 +8,25 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     
   
-    <nav class="navbar navbar-dark bg-dark"">
+<nav class="navbar" style="background-color: #e3f2fd;">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Doctors List </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <a class="navbar-brand" href="#">Doctors List</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Welcome to Homework 6</a>
+          <a class="nav-link active" aria-current="page" href="index.html">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="index.php">Home</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Libraries
-          </a>
-          <ul class="dropdown-menu">
-            
-            <li><a class="dropdown-item" href="index.html"> Home</a></li>
-            <li><a class="dropdown-item" href="Doctor-Table.php "> Doctors </a></li>
-            <li><a class="dropdown-item" href="Patient-Table.php"> Patients</a></li>
-            <li><a class="dropdown-item" href="#"> Request an appointment</a></li>
-  >
-          
-            
-          </ul>
+          <a class="nav-link" href="Doctor-Table.php">Doctors</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
+          <a class="nav-link" href="Patient-Table.php">Patients</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="ajax-load/form-appointment.html">Request an appointment</a>
         </li>
       </ul>
       <form class="d-flex" role="search">
