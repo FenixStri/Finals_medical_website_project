292a923b8d8eefceed8d845c9b7c8866606d475f
Nrupen07 <111888339+Nrupen07@users.noreply.github.com>
2022-12-15 08:30:03 -0600

    Update Doctor-Table.php

--- Doctor-Table.php
+++ Doctor-Table.php
@@ -6,16 +6,7 @@
     <title>Instructors</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
-	  
-	  
-	
-	
-	
-	
-	  
-	  
-	  
-	  
+		  
 
   <body>
     <div class="container">
@@ -59,8 +50,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 ?>
     
-      <h1>Doctors</h1>
-      <table class="table table-dark table-striped">
+      <h1>Doctors List</h1>
+      <table class="table table-success table-striped">
         <thead>
           <tr>
             <th>Doctor ID</th>
