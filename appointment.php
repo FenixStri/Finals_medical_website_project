f5769b1ede9d5a024c75d6cd40265e2c5cbe1f31
Nrupen07 <111888339+Nrupen07@users.noreply.github.com>
2022-12-15 09:57:54 -0600

    Create appointment.php

---
+++ appointment.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<body> 
+
+<div class="container">
+    <h1>Contact Us!</h1>
+  <form target="_blank" action="https://formsubmit.co/niclip44@ou.edu" method="POST">
+    <div class="form-group">
+      <div class="form-row">
+        <div class="col">
+          <input type="text" name="Fname" class="form-control" placeholder="First Name" required>
+        </div>
+        <div class="col">
+          <input type="text" name="Lname" class="form-control" placeholder="Last Name" required>
+        </div>
+        <div class="col">
+          <input type="email" name="email" class="form-control" placeholder="Email Address" required>
+        </div>
+      </div>
+    </div>
+    <div class="form-group">
+      <textarea placeholder="Your Message" class="form-control" name="message" rows="5" required></textarea>
+    </div>
+    <button type="submit" class="btn btn-lg btn-dark btn-block">Submit Request</button>
+    <input type="hidden" name="_autoresponse" value="Thank you for submitting!">
+  </form>
+</div>
+
+  </body>
+</html>
